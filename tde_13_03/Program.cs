87114977b18238040691334822bdc00bea4781fa
Alfredo Mello <alfredomello@rede.ulbra.br>
2024-03-17 16:59:39 -0300

    Add files via upload

---
+++ tde_13_03/Program.cs
@@ -0,0 +1,45 @@
+﻿//* Questão - 1 :
+int idade = 35;
+Console.WriteLine(idade);
+
+//* Questão - 2 :
+string nome = "Maria";
+Console.WriteLine(nome);
+
+//* Questão - 3 :
+double altura = 3.45;
+Console.WriteLine(altura);
+
+//* Questão - 4 :
+const int ano = 12;
+Console.WriteLine(ano);
+
+//* Questão - 5 :
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+//* Questão - 6 : Variáveis de tipos de referência armazenam referências em seus dados (objetos) 
+//* enquanto que variáveis de tipos de valor contém diretamente seus dados.
+//* referenciado pela outra variável. Com tipos de valor, cada variável tem a própria cópia dos dados 
+//* e as operações em uma variável não podem afetar a outra (exceto no caso das variáveis de parâmetros in, ref e out)
+
+//* Questão - 7 : Um nullable type (tipo nullable) em C# é um tipo de dado que pode representar um valor do tipo original,
+//* juntamente com o valor null. Isso permite que o tipo de dado represente um intervalo maior de valores,
+//* incluindo a ausência de um valor válido.
+
+//* Utilidade do Type NUllable: Usa-se um tipo de valor anulável quando precisa representar o valor indefinido de um
+//* tipo de valor subjacente.
+
+//* Questão - 8 : É uma convenção de nomenclatura usada na programação para formar identificadores (nomes de variáveis, funções, etc.)
+//* em que várias palavras são combinadas sem espaços e cada palavra subsequente além da primeira começa com uma letra maiúscula
+//* lembrando a corcova de um camelo. Exemplo : escapeFromTarkov .
+
+//* Questão - 9 : É uma convenção de nomenclatura usada para formar identificadores (nomes de classes, métodos, propriedades, etc.)
+//* em que várias palavras são combinadas sem espaços e cada palavra começa com uma letra maiúscula, incluindo a primeira palavra.
+//* Exemplo : MikeIronTyson.
+
+//* Questão - 10 :
+
+int x = 77;
+int y = 66;
+Console.WriteLine($"O resultado da soma de x e y é { x + y} .");
